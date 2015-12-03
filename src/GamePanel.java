f3208bc5418991d14b6f260198197f6628bc0347
Arend Peter <apc1993@gmail.com>
2015-12-03 09:49:35 -0500

    Drew Rectangles

--- src/GamePanel.java
+++ src/GamePanel.java
@@ -1,3 +1,6 @@
+import java.awt.Color;
+import java.awt.Graphics;
+
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 
@@ -10,7 +13,21 @@ public class GamePanel extends JPanel {
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		frame.setLocationRelativeTo(null);
 		
+		GamePanel panel = new GamePanel();
+		
+		frame.add(panel);
+		
 		frame.setVisible(true);
 	}
+	
+	public GamePanel(){}
 
+	@Override
+	protected void paintComponent(Graphics g) {
+		g.setColor(Color.WHITE);
+		g.fillRect(0, 0, getWidth(), getHeight());
+
+		g.setColor(Color.ORANGE);
+		g.fillRect(10,20,50,50);
+	}
 }
